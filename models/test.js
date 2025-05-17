f09553b3900737a98c673622a2c9618674a99b16
Malik <ahst30@gmail.com>
2025-05-17 05:15:14 +0300

    fix: remove Sequelize from sanity test

--- models/test.js
+++ models/test.js
@@ -1,28 +1,6 @@
+// models/test.js
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Test extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Test.init({
-    title: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    duration: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Test',
-  });
-  return Test;
-};
+
 describe('Sanity check', () => {
   test('true is truthy', () => {
     expect(true).toBe(true);
