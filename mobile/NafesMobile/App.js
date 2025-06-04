0e308edd2e9bdda0eb94ba887d948b9761f5af0a
Malik <ahst30@gmail.com>
2025-06-05 01:37:42 +0300

    Remove nested screens folder and fix imports in App.js

--- mobile/NafesMobile/App.js
+++ mobile/NafesMobile/App.js
@@ -1,18 +1,26 @@
-import * as React from 'react';
+// File: C:/Users/ahst3/Desktop/NafesApp/mobile/NafesMobile/App.js
+
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import WelcomeScreen from './screens/WelcomeScreen';
-import LoginScreen   from './screens/LoginScreen';
+import LoginScreen from './screens/LoginScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Welcome" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Welcome" component={WelcomeScreen} />
-        <Stack.Screen name="Login"   component={LoginScreen} />
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen 
+          name="Welcome" 
+          component={WelcomeScreen} 
+        />
+        <Stack.Screen 
+          name="Login" 
+          component={LoginScreen} 
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
